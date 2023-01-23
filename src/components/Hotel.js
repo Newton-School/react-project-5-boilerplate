7c6e1250d8c150cfe670c3e18bf3b5bf731f6a71
kunal-1388 <90197041+kunal-1388@users.noreply.github.com>
2023-01-23 15:13:36 +0530

    Done

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,33 +1,19 @@
 import React, { useState } from "react";
 import "../styles/Hotel.css";
 function Hotel(props) {
-    const { id, name, location, price, img } = props.data;
-    function handleClick(e) {
-        let savedList =
-            JSON.parse(window.localStorage.getItem("favourites")) || [];
-
-        if (savedList.find((item) => item.id === id) === undefined) {
-            savedList.push({ id, name, location, price, img });
-            window.localStorage.setItem(
-                "favourites",
-                JSON.stringify(savedList)
-            );
-        }
-    }
+    function handleClick(e) {}
 
     return (
         <div className="card">
             <div className="card-image">
-                <img src={img} />
+                <img />
             </div>
             <div className="card-text">
-                <h2>{name}</h2>
-                <span className="location"> {location}</span>
-                <h4>Rs. {price} per night</h4>
+                <h2></h2>
+                <span className="location"></span>
+                <h4></h4>
             </div>
-            <button className={`button`} onClick={handleClick}>
-                Add to Favourites
-            </button>
+            <button className={`button`}>Add to Favourites</button>
         </div>
     );
 }
